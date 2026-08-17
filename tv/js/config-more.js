045856ccfc11da3287b0176ee65562fb7fe07ba7
J T <jor.teron@gmail.com>
2026-08-18 01:15:58 +0530

    Add files via upload

---
+++ tv/js/config-more.js
@@ -0,0 +1,72 @@
+const SPECIAL_TILES = [
+  {
+    num: 1,
+    name: "Home",
+    type: "page",
+    action: "tv-v3.html",
+    logo: "../image/home.png"
+  },
+  {
+    num: 2,
+    name: "YouTube",
+    type: "app",
+    action: "vnd.youtube://",
+    logo: "../image/youtube.png"
+  },
+  {
+    num: 3,
+    name: "Kids",
+    type: "page",
+    action: "kids-mini.html",
+    logo: "../image/golf.png"
+  },
+  {
+    num: 4,
+    name: "Kids (All)",
+    type: "page",
+    action: "kids.html",
+    logo: "../image/chase.png"
+  },
+  {
+    num: 5,
+    name: "India",
+    type: "page",
+    action: "india.html",
+    logo: "../image/india.png"
+  },
+  {
+    num: 6,
+    name: "World",
+    type: "page",
+    action: "world.html",
+    logo: "../image/earth.png"
+  },
+  {
+    num: 7,
+    name: "Photos",
+    type: "page",
+    action: "photo.html",
+    logo: "../image/gallery.png"
+  },
+  {
+    num: 8,
+    name: "Videos",
+    type: "page",
+    action: "video.html",
+    logo: "../image/play.png"
+  },
+  {
+    num: 9,
+    name: "Play Store",
+    type: "app",
+    action: "intent://com.android.vending/#Intent;scheme=android-app;end",
+    logo: "../image/app.png"
+  },
+  {
+    num: 10,
+    name: "Settings",
+    type: "app",
+    action: "intent://com.android.settings/#Intent;scheme=android-app;end",
+    logo: "../image/setting.png"
+  }
+];
--- tv/js/config.js
+++ tv/js/config.js
@@ -10,63 +10,28 @@ const SPECIAL_TILES = [
     num: 2,
     name: "Kids",
     type: "page",
+    action: "kids-mini.html",
+    logo: "../image/golf.png"
+  },
+  {
+    num: 3,
+    name: "Kids (All)",
+    type: "page",
     action: "kids.html",
     logo: "../image/chase.png"
   },
   {
-    num: 3,
+    num: 4,
     name: "India",
     type: "page",
     action: "india.html",
     logo: "../image/india.png"
   },
   {
-    num: 4,
-    name: "World",
-    type: "page",
-    action: "world.html",
-    logo: "../image/earth.png"
-  },
-  {
     num: 5,
-    name: "Photos",
+    name: "More",
     type: "page",
-    action: "photo.html",
-    logo: "../image/gallery.png"
-  },
-  {
-    num: 6,
-    name: "Videos",
-    type: "page",
-    action: "video.html",
-    logo: "../image/play.png"
-  },
-  {
-    num: 7,
-    name: "",
-    type: "empty",
-    action: "",
-    logo: "../image/folder.png"
-  },
-  {
-    num: 8,
-    name: "",
-    type: "empty",
-    action: "",
-    logo: "../image/folder.png"
-  },
-  {
-    num: 9,
-    name: "Play Store",
-    type: "app",
-    action: "intent://com.android.vending/#Intent;scheme=android-app;end",
-    logo: "../image/app.png"
-  },
-  {
-    num: 10,
-    name: "Settings",
-    type: "app",
-    action: "intent://com.android.settings/#Intent;scheme=android-app;end",
-    logo: "../image/setting.png"
+    action: "tv-v3-more.html",
+    logo: "../image/menu.png"
   }
 ];
